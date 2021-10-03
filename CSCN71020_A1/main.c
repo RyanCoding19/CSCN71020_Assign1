fcf6aa2fc1687fc43bfe87ca50e5ac1a910451ec
RyanCoding19 <Ryan_Wallace2019@outlook.com>
2021-10-03 15:26:19 -0400

    actual subtraction one since i don't know how to delete

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -14,7 +14,7 @@ void main() {
 	int inputNum;
 
 	printf("Enter operation number: ");
-	scanf_s("%1o", &inputNum);
+	scanf_s("%d", &inputNum);
 
 	switch (inputNum)
 	{
@@ -50,11 +50,11 @@ void add() {
 
 void subtract() {
 	double num1, num2, result;
-	printf("Enter your first value:");
-	scanf_s("%1f", &num1);
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
 	printf("Enter the second value:");
-	scanf_s("%1f", &num2);
+	scanf_s("%lf", &num2);
 	result = num1 - num2;
-	printf("%1f - %1f = %1f\n", num1, num2, result);
+	printf("%lf - %lf = %lf\n", num1, num2, result);
 
 }
